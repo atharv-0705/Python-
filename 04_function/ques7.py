ac71f354ad1ad7f4a5c3a53d8445c9c21f0de89f
Atharv Gupta <atharvgupta0705@gmail.com>
2026-06-29 21:12:26 +0530

    Learn Function

--- 04_function/ques7.py
+++ 04_function/ques7.py
@@ -1,12 +1,12 @@
 # FUNCTION WITH *args
 '''Write a function that takes variable number of arguments and return their sum'''
 
-def sum_all(*args):
+def sum_all(*args): #*args is used to pass variable number of arguments
     print(args)
     for i in args:
         print(i * 2)
-    return sum(args)
-    return sum(args)
+    return sum(args)     
+    return sum(args) #sum() function is used to calculate sum of all elements in an iterable
 
 print(sum_all(1,2))
 print(sum_all(1,2,3,4,5))
--- 04_function/ques9.py
+++ 04_function/ques9.py
@@ -1,10 +1,10 @@
 # GENERATOR FUNCTION WITH YIELD
 '''Write a generator function that yields even numbers up to a specified limit'''
 
-limit = 10
+
 def even_generator(limit):
-    li = []
     for i in  range(2, limit + 1,2):
-        li
-    return i
-    
+        yield i
+
+for num in even_generator(10):
+    print(num)
